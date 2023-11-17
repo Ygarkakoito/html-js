542770edd9f3d6e75b78e1a2a1bb3094012885e8
Ygarkakoito <102905043+Ygarkakoito@users.noreply.github.com>
2023-11-17 13:23:59 +0300

    Create password.js

---
+++ social-network/password.js
@@ -0,0 +1,15 @@
+function generateRandomPassword(length) {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+const passwordLength = 8;
+const randomPassword = generateRandomPassword(passwordLength);
+console.log("Случайный пароль:", randomPassword);
